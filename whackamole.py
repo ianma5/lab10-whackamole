3618c3e734ada14dae5abca1b01bfdc153cf97a6
Ian Maksimovic <ianmaksimovic@gmail.com>
2024-11-13 13:37:52 -0500

    completed

--- whackamole.py
+++ whackamole.py
@@ -1,36 +1,45 @@
 import pygame
+import random
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
-        
+        molepos = (0,0) # initial position is top left corner
+        screen = pygame.display.set_mode((640, 512)) # 20x16, 32x32 squares
+        square_size = 32
         clock = pygame.time.Clock()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if (event.pos[0]//square_size==molepos[0]//square_size) and (event.pos[1]//square_size==molepos[1]//square_size): # checks if mouseclick divided by square size is equalto random molepos
+                        molepos = randomlocation(square_size) # sets the position so he is on a random square
+
             screen.fill("light green")
-            draw_rows(screen)
-            draw_cols(screen)
-            
+        
+            #draws the rows and columns onto the screen
+            draw_rows(screen, square_size)
+            draw_cols(screen, square_size)
+            # places/updates the mole somewhere on the screen
+            screen.blit(mole_image, mole_image.get_rect(topleft=molepos))
             pygame.display.flip()
             clock.tick(60)
     finally:
         pygame.quit()
 
-def draw_rows(screen):
+def draw_rows(screen, square_size):
     for row in range(16):
-        print("hi")
-        pygame.draw.line(screen, (255,255,255), (0,row*32), (640,row*32))
-def draw_cols(screen):
+        pygame.draw.line(screen, (255,255,255), (0,row*square_size), (640,row*square_size))
+def draw_cols(screen, square_size):
     for col in range(20):
-        print("hi")
-        pygame.draw.line(screen, (255,255,255), (col*32,0), (col*32,512))
+        pygame.draw.line(screen, (255,255,255), (col*square_size,0), (col*square_size,512))
+def randomlocation(square_size):
+    row = random.randint(0,19)
+    col = random.randint(0,15)
+    return (row*square_size,col*square_size) # returns tuple multiplied by square size
 
 if __name__ == "__main__":
     main()
