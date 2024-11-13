8310f9c878b8fff043270d7388f1d6de4cefd481
Ian Maksimovic <ianmaksimovic@gmail.com>
2024-11-13 00:45:47 -0500

    grid

--- whackamole.py
+++ whackamole.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 def main():
     try:
         pygame.init()
@@ -8,6 +7,7 @@ def main():
         # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
+        
         clock = pygame.time.Clock()
         running = True
         while running:
@@ -15,11 +15,22 @@ def main():
                 if event.type == pygame.QUIT:
                     running = False
             screen.fill("light green")
+            draw_rows(screen)
+            draw_cols(screen)
+            
             pygame.display.flip()
             clock.tick(60)
     finally:
         pygame.quit()
 
+def draw_rows(screen):
+    for row in range(16):
+        print("hi")
+        pygame.draw.line(screen, (255,255,255), (0,row*32), (640,row*32))
+def draw_cols(screen):
+    for col in range(20):
+        print("hi")
+        pygame.draw.line(screen, (255,255,255), (col*32,0), (col*32,512))
 
 if __name__ == "__main__":
     main()
